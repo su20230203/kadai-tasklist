94ef0135c06548bad8a626cf2b761c4d38c62fd6
chie.sugawara2 <sugawara.chie@tis-h.co.jp>
2023-02-15 11:34:05 +0900

    getAllTasksを追加

--- src/models/Task.java
+++ src/models/Task.java
@@ -9,7 +9,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
 @Entity
+@NamedQueries({
+@NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+    )
+})
 @Table(name = "tasks")
 
 public class Task {
